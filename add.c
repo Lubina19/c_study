4c706f68ae351d90935df0c9cc02cb4adde8da1b
Lubina19 <lubina19@gmail.com>
2018-10-07 11:51:03 +0530

    cleanup white space

--- add.c
+++ add.c
@@ -3,17 +3,11 @@ int main()
 {
 int num1,num2,sum;
 
-
-
-
-
-
 printf("Enter the first number to add:");
 scanf("%d", &num1);
-
 printf("Enter the second number to add:");
-
 scanf("%d", &num2);
+
 sum = num1 + num2;
 printf("Sum = %d\n", sum);
 }
